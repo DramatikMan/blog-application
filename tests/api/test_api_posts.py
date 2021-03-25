ca505b708bad9c1142c83e283163ea06554a96a5
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-26 00:26:18 +0300

    Add test: test_posts_POST_201

--- tests/api/test_api_posts.py
+++ tests/api/test_api_posts.py
@@ -2,26 +2,26 @@ import pytest
 
 
 @pytest.mark.parametrize('post_id', ['', '100'])
-def test_posts_get_200(client, post_id):
+def test_posts_GET_200(client, post_id):
     response = client.get('/api/post/' + post_id)
     assert response.status_code == 200
 
 
-def test_posts_get_404(client):
+def test_posts_GET_404(client):
     response = client.get('/api/post/101')
     assert response.status_code == 404
 
 
-def test_posts_post_405(client):
+def test_posts_POST_405(client):
     response = client.post('/api/post/101')
     assert response.status_code == 405
 
 
-# def test_posts_post(client):
-#     payload = {
-#         'title': 'Post 101',
-#         'text': 'Example text',
-#         'tags': 'pytest'
-#     }
-#     response = client.post('/api/post/', json=payload)
-#     assert 101, response.status_code == [reponse.data['new_post.id'], 201]
+def test_posts_POST_201(client):
+    payload = {
+        'title': 'Post 101',
+        'text': 'Example text',
+        'tags': 'pytest'
+    }
+    response = client.post('/api/post/', json=payload)
+    assert 101, response.status_code == [reponse.data['new_post.id'], 201]
