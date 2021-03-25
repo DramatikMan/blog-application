d15367cea1ff08aa0a3cc9fd3263884c3b72d2b7
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-26 00:23:00 +0300

    Add db re-init capabilities to the app fixture

--- tests/api/test_api_posts.py
+++ tests/api/test_api_posts.py
@@ -10,3 +10,18 @@ def test_posts_get_200(client, post_id):
 def test_posts_get_404(client):
     response = client.get('/api/post/101')
     assert response.status_code == 404
+
+
+def test_posts_post_405(client):
+    response = client.post('/api/post/101')
+    assert response.status_code == 405
+
+
+# def test_posts_post(client):
+#     payload = {
+#         'title': 'Post 101',
+#         'text': 'Example text',
+#         'tags': 'pytest'
+#     }
+#     response = client.post('/api/post/', json=payload)
+#     assert 101, response.status_code == [reponse.data['new_post.id'], 201]
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,24 @@ import os
 import pytest
 
 from blog_application import create_app
+from blog_application.commands import db_fill, db_clear
 
 
 @pytest.fixture(scope='session')
 def app():
     app = create_app()
-    return app
+    cli = app.test_cli_runner()
+    cli.invoke(db_fill)
+    yield app
+
+    cli.invoke(db_clear)
 
 
 @pytest.fixture(scope='session')
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture(scope='session')
+def runner(app):
+    return app.test_cli_runner()
