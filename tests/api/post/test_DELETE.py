f9bc4480d195bff162bfe7c2dc599864eaa18360
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-26 18:07:30 +0300

    Add test: DELETE at /post/<id> with bad token

--- tests/api/post/test_DELETE.py
+++ tests/api/post/test_DELETE.py
@@ -12,3 +12,9 @@ def test_DELETE_400_bad_payload(client):
     payload = {'bad_payload': 'value'}
     response = client.delete('/api/post/1')
     assert response.status_code == 400
+
+
+def test_DELETE_401_bad_token(client):
+    payload = {'token': 'wrong'}
+    response = client.delete('/api/post/1', json=payload)
+    assert response.status_code == 401
