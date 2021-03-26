d489744d74ec41a1268d0b53d06a6024e2ca6275
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-26 17:49:34 +0300

    Add test: GET at /post/ with page number

--- tests/api/post/test_GET.py
+++ tests/api/post/test_GET.py
@@ -7,6 +7,12 @@ def test_GET_200_all_or_id(client, post_id):
     assert response.status_code == 200
 
 
+def test_GET_200_all_page(client):
+    payload = {'page': 2}
+    response = client.get('/api/post/', json=payload)
+    assert response.status_code == 200
+
+
 def test_GET_404_bad_id(client):
     response = client.get('/api/post/101')
     assert response.status_code == 404
