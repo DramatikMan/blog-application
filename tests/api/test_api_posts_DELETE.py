e4ec610ca6c5eefdb6d1a6e9d4a384daa3f13074
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-26 15:51:48 +0300

    Moved to subfolder

--- tests/api/test_api_posts_DELETE.py
+++
@@ -1,8 +0,0 @@
-def test_DELETE_405_no_id(client):
-    response = client.delete('/api/post/')
-    assert response.status_code == 405
-
-
-def test_DELETE_404_bad_id(client):
-    response = client.delete('/api/post/999')
-    assert response.status_code == 404
