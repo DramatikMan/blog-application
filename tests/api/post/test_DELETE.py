0450d79a1f866fc68d646d45811844120ba21d2a
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-26 18:00:32 +0300

    Add test: DELETE at /post/ with wrong data

--- tests/api/post/test_DELETE.py
+++ tests/api/post/test_DELETE.py
@@ -6,3 +6,9 @@ def test_DELETE_405_no_id(client):
 def test_DELETE_404_bad_id(client):
     response = client.delete('/api/post/999')
     assert response.status_code == 404
+
+
+def test_DELETE_400_bad_payload(client):
+    payload = {'bad_payload': 'value'}
+    response = client.delete('/api/post/1')
+    assert response.status_code == 400
