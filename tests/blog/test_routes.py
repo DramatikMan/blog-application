1e68161459766dbdd8f97b67735cc9c521e10fe8
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-27 18:49:01 +0300

    Add test: adding comments to posts

--- tests/blog/test_routes.py
+++ tests/blog/test_routes.py
@@ -12,3 +12,15 @@ def test_post_page(client):
     response = client.get('/blog/post/100')
     assert b'Written by' in response.data
     assert b'New comment' in response.data
+
+
+def test_add_comment(app, client):
+    url = '/blog/post/100'
+    with app.test_request_context(url):
+        form = CommentForm()
+        form.name.data = 'tester'
+        form.text.data = 'Example comment'
+        client.post(url, data=form.data)
+    with app.app_context():
+        added_comment = Comment.query.filter_by(text='Example comment').first()
+    assert added_comment
