aaa029946c11ed96912cd7d2a756fca00cfc996d
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-27 13:36:29 +0300

    Add test: GET specific post page

--- tests/blog/test_routes.py
+++ tests/blog/test_routes.py
@@ -2,3 +2,9 @@ def test_home(client):
     response = client.get('/blog')
     assert b'Recent Posts' in response.data
     assert b'Popular Tags' in response.data
+
+
+def test_specific_post(client):
+    response = client.get('/blog/post/100')
+    assert b'Written by' in response.data
+    assert b'New comment' in response.data
