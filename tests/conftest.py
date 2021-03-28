d5be09e7a809260ed6d6d1b507eb656b47397a29
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-28 22:11:23 +0300

    Fix environment setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 import pytest
+from sqlalchemy import text
 
 from blog_application import create_app
 from blog_application.commands import db_fill, db_clear
+from blog_application.models import db
 
 
 @pytest.fixture(scope='session')
 def app():
     app = create_app()
+    with app.app_context():
+        db.engine.execute(text('CREATE SCHEMA IF NOT EXISTS test'))
     cli = app.test_cli_runner()
     cli.invoke(db_clear)
     cli.invoke(db_fill)
