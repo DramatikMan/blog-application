098325311a3e23822757211374c7aa080feb1f4f
Sergey Pavlov <sergeypavlov444@gmail.com>
2021-03-30 12:26:37 +0300

    Add test for models' __repr__()

---
+++ tests/test_orm.py
@@ -0,0 +1,14 @@
+from blog_application.models import User, Post, Comment, Tag, Role
+
+def test_repr(app):
+    with app.app_context():
+        user = User.query.filter_by(username='Random_User').one()
+        post = Post.query.filter_by(id=33).one()
+        comment = Comment.query.filter_by(text='Example comment').first()
+        tag = Tag.query.filter_by(title='Flask').one()
+        role = Role.query.filter_by(name='poster').one()
+    assert str(user) == f"<User '{user.username}'>"
+    assert str(post) == f"<Post '{post.title}'>"
+    assert str(comment) == f"<Comment '{comment.text[:15]}'>"
+    assert str(tag) == f"<Tag '{tag.title}'>"
+    assert str(role) == f"<Role '{role.name}'>"
