2b71234e62977f3c9415e07bd3e09e6ad47d2f91
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-13 23:54:42 +0300

    Update gunicorn script

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,11 +1,9 @@
 pipenv install gunicorn
 
 echo $'
-pythonpath="/project"
 workers=4
-bind="127.0.0.1:8080"
+bind="0.0.0.0:8080"
 wsgi_app="blog_application:create_app()"' > gunicorn.conf.py
 
 source .venv/bin/activate
-flask db_clear && flask db_fill
 gunicorn
