c19ff3d3f09c00f3a014ae14dd8c2b3dffdcab24
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-14 19:25:47 +0300

    Switch deployment style to a unix socket through Docker volumes

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -2,7 +2,7 @@ pipenv install gunicorn
 
 echo $'
 workers=4
-bind="0.0.0.0:8080"
+bind="unix:/tmp/blog_app/gunicorn.sock"
 wsgi_app="blog_app:create_app()"' > gunicorn.conf.py
 
 source .venv/bin/activate
