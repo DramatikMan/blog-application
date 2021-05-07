30b2eca987d0e6ad2c5141786562e0d50b5355c9
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-07 21:07:49 +0300

    Switch deployment from unix socket to TCP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,7 +27,7 @@ pipenv install gunicorn
 
 echo $'
 workers=4
-bind="unix:/tmp/blog_app/gunicorn.sock"
+bind="0.0.0.0:8000"
 wsgi_app="blog_app:create_app()"' > gunicorn.conf.py
 
 source .venv/bin/activate
